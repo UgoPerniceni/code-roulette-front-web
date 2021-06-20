954aaecdaf80e69c4117fff823ae3d551bd9eb75
PERNICENI Ugo <ugo.perniceni@hotmail.fr>
2021-06-20 15:05:56 +0200

    introduce rank system :sparkles:

--- src/app/component/rank/rank.component.ts
+++ src/app/component/rank/rank.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
+import {UserService} from '../../service/user.service';
+import {User} from '../../model/User';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {Exercise} from '../../model/Exercise';
 
 @Component({
   selector: 'app-rank',
   templateUrl: './rank.component.html',
   styleUrls: ['./rank.component.css']
 })
-export class RankComponent implements OnInit {
+export class RankComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  dataSource = new MatTableDataSource<User>();
+  displayedColumns: string[] = ['elo', 'email', 'userName'];
+
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.refreshDataSource();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
+
+  refreshDataSource(): void {
+    this.userService.getUsers().subscribe(data => {
+      console.log(data);
+      this.dataSource.data = data;
+    });
   }
 
 }
--- src/app/model/User.ts
+++ src/app/model/User.ts
@@ -6,14 +6,18 @@ export class User {
   userName: string;
   password: string;
   birthDate: Date;
+  elo: number;
+  elo_problems: number;
 
-  constructor(id: string, firstName: string, lastName: string, email: string, userName: string, password: string, birthDate: Date) {
+  constructor(id: string, firstName: string, lastName: string, email: string, userName: string, password: string, elo: number, elo_problems: number, birthDate: Date) {
     this.id = id;
     this.firstName = firstName;
     this.lastName = lastName;
     this.email = email;
     this.userName = userName;
     this.password = password;
+    this.elo = elo;
+    this.elo_problems = elo_problems;
     this.birthDate = birthDate;
   }
 }
