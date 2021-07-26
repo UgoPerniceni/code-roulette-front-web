599425e4c1df8ad4a3dd6e2d82a8a6d172a5016d
PERNICENI Ugo <ugo.perniceni@hotmail.fr>
2021-07-26 16:12:02 +0200

    update ip :wrench:

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
 
-  apiUrl: 'http://34.253.194.75:80/api/',
-  socketUrl: 'http://34.253.194.75:80/ws'
+  apiUrl: 'http://34.248.4.98:80/api/',
+  socketUrl: 'http://34.248.4.98.75:80/ws'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
 
   // apiUrl: 'http://localhost:8080/api/',
   // socketUrl: 'http://localhost:8080/ws/',
-  apiUrl: 'http://34.253.194.75:80/api/',
-  socketUrl: 'http://34-253-194-75:80/ws/',
+  apiUrl: 'http://34.248.4.98:80/api/',
+  socketUrl: 'http://34.248.4.98:80/ws/',
 };
 
 /*
